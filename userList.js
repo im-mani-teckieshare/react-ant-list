00b43a3de62ddafcc98300080a36be0acb5c2ba6
im-mani-teckieshare <teckieshare@gmail.com>
2020-03-22 21:34:49 +0530

    1 file modified

--- userList.js
+++ userList.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import { List, message, Avatar, Spin } from 'antd';
+import { List, message, Avatar, Spin, Button, Icon } from 'antd';
+import { SmallDashOutlined } from '@ant-design/icons';
 
 import reqwest from 'reqwest';
 
@@ -11,13 +12,15 @@ export default class UserList extends React.Component{
 
 state = {
   dataList:[],
-  loading: false
+  loading: true,
+  loadMore: true
 }
 
 componentDidMount(){
   this.fetchData(res=> {
       this.setState({
         dataList: res.results,
+        loading:false
       });
     });
 }
@@ -44,12 +47,48 @@ renderItem = item => {
       </List.Item>
 }
 
+onLoadMore = ()=>{
+  this.setState({
+    loading: true
+  });
+  this.fetchData(res=>{
+    const {dataList} = this.state;
+    dataList = [...dataList,...res.results];
+
+    if(dataList.length > 40){
+      this.setState({
+        loadMore:false
+      })
+    }
+    this.setState({
+      dataList,
+      loading:false
+    });
+  })
+
+}
 render(){
-  const {dataList} = this.state;
+  const {dataList,loadMore,loading} = this.state;
+
+  const showLoadMore = loadMore ?  (
+        <div
+          style={{
+            textAlign: 'center',
+            marginTop: 12,
+            height: 32,
+            lineHeight: '32px',
+          }}
+        >
+          <Button type="link" size="large" onClick={this.onLoadMore}><SmallDashOutlined style={{fontSize:"2.2rem"}} /></Button>
+        </div>
+      ) : null;
 return  (<List
     itemLayout="horizontal"
     dataSource={dataList}
+    header = {"Header"}
     renderItem={this.renderItem}
+    loadMore={showLoadMore}
+    loading={loading}
   />)
 }
 
